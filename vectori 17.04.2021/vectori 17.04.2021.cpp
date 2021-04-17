bb4a6be7e6511f64eefeeeee54b3da42002cf018
Andrei2022 <andrei.dorin09@yahoo.com>
2021-04-17 11:42:42 +0300

    Add meditatii 17.04.2021

---
+++ vectori 17.04.2021/vectori 17.04.2021.cpp
@@ -0,0 +1,28 @@
+﻿//Să se scrie un program care ordonează descrescător elementele unui vector.
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+void swap(int &a, int &b)
+{
+	int aux = a;
+	a = b;
+	b = aux;
+}
+
+int main()
+{
+	int v[1000] , i,j, n, aux;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		scanf("%d", &v[i]);
+
+	for(i=1;i<n;i++)
+		for(j=i+1;j<=n;j++)
+			if (v[i] < v[j])
+			{
+				swap(v[i], v[j]);
+			}
+	for (i = 1; i <= n; i++)
+		printf("%d ", v[i]);
+}
