bc9fe8ea8b4d40985706a3561910e0975ef74752
Andrei2022 <andrei.dorin09@yahoo.com>
2021-04-17 11:44:50 +0300

    Delete meditatii 17.04.2021 and add a problem

---
+++ divizori 2.cpp
@@ -0,0 +1,21 @@
+﻿//Se citește un număr natural n. 
+//Să se determine câți divizori pari are acest număr.
+
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+
+int main()
+{
+	int div, i, n, contor=0, suma = 0;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		if (n % i == 0 && i % 2 == 0) 
+		{
+			contor++;
+		}
+	}
+	printf("Exista %d dizivori pari", contor);
+
+}
